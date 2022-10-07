52bc7d83d4632e3e8e0ce1dcca043dac64299333
seokminqwerty <lsmseokminjjang@gmail.com>
2022-10-07 13:51:47 +0900

    회원가입, 로그인 구현하기

---
+++ account/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+# Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, name, username, email, password=None):
+        if not name:
+            raise ValueError('이름을 입력해야 합니다.')
+        if not username:
+            raise ValueError('닉네임을 입력해야 합니다.')
+        if not email:
+            raise ValueError('이메일을 입력해야 합니다.')
+        user = self.model(
+            name = name,
+            username = username,
+            email = self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, name, username, email, password=None):
+        user = self.create_user(
+            name = name,
+            username = username,
+            email = self.normalize_email(email),
+            password = password
+        )
+        user.is_admin = True
+        user.save(using=self.db)
+        return user
+
+
+class User(AbstractBaseUser):
+    name = models.CharField(default='', max_length=20, null=False, blank=False)
+    username = models.CharField(default='', max_length=100, null=False, blank=False, unique=True)
+    email = models.EmailField(default='', max_length=100, null=False, blank=False, unique=True)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name', 'email']
+
+    def __str__(self):
+        return self.username
+
+
+
+
---
+++ account/serializers.py
@@ -0,0 +1,15 @@
+from .models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            name=validated_data['name'],
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password']
