2058fa4e80ca92b1f5fff308524481261617f220
chlehdrb5 <chlehdrb0513@gmail.com>
2022-10-09 16:01:46 +0900

    authentication 추가

--- paint_int_prj/settings.py
+++ paint_int_prj/settings.py
@@ -128,3 +128,14 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.User'
+
+# authentication
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+}
--- posts/serializers.py
+++ posts/serializers.py
@@ -3,6 +3,7 @@ from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.name')
     class Meta:
         model = Post
-        fields = ('id', 'title', 'content', 'created_at', 'updated_at')
+        fields = ('id', 'title', 'content', 'author', 'created_at', 'updated_at')
