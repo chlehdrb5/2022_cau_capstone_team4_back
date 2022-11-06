72e191a07158171f0813590d5171932b895775c9
chlehdrb5 <chlehdrb0513@gmail.com>
2022-11-06 21:18:44 +0900

    CORS_ALLOW 설정 변경

--- paint_int_prj/settings.py
+++ paint_int_prj/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-5$!=m%-lwf2wq_enatx#3f3nxs=c(e4zluvb2c@i1g@vb!w2wg
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '3.37.160.197',
+    '*'
+    # '3.37.160.197',
 ]
 
 
@@ -58,10 +59,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = [
-    'http://3.37.160.197',
-    'http://15.165.74.137:8000',
-]
+CORS_ORIGIN_ALLOW_ALL = True
+
+# CORS_ORIGIN_WHITELIST = [
+#     'http://3.37.160.197',
+#     'http://15.165.74.137:8000',
+# ]
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'paint_int_prj.urls'
